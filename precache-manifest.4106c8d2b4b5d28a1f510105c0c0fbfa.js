5a370283a270feb5c1009f72d1e8665c9e4adb20
Maciej Bojko <maciej.bojko@gmail.com>
2019-05-30 22:42:15 +0200

    Updates

---
+++ precache-manifest.4106c8d2b4b5d28a1f510105c0c0fbfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d190702fe0c12c8a777d7d2686c7e4fe",
+    "url": "/photobooth/index.html"
+  },
+  {
+    "revision": "741241682d1a2c35ea3d",
+    "url": "/photobooth/static/css/main.de68eb5d.chunk.css"
+  },
+  {
+    "revision": "d8045654850bb0c86830",
+    "url": "/photobooth/static/js/2.d242ceb2.chunk.js"
+  },
+  {
+    "revision": "741241682d1a2c35ea3d",
+    "url": "/photobooth/static/js/main.4734617c.chunk.js"
+  },
+  {
+    "revision": "d87049bdc654a27be3da",
+    "url": "/photobooth/static/js/runtime~main.11004836.js"
+  }
+]);
